0890b57c64e32ed225275e9ba756cd7f049280f0
kilokomuli <kilokoemma175@gmail.com>
2023-09-16 14:27:26 -0700

    local buffer

--- main.h
+++ main.h
@@ -5,8 +5,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#define BUFF_SIZE 1024
 
 int _printf(const char *format, ...);
 
+int get_width(const char *format, int *i, va_list list);
+
+/***Utils***/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif
