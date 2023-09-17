decf3301b0733806d113543d8cd2e7bc1d4bf8bc
Cheto02 <fredycheto@gmail.com>
2023-09-17 05:22:17 -0700

    betty corrections to 2-binary_func.c

--- 2-binary_func.c
+++ 2-binary_func.c
@@ -9,24 +9,24 @@
  */
 int print_b(va_list args)
 {
-    unsigned int n = va_arg(args, unsigned int);
-    char *binary = malloc(sizeof(char) * 33);
-    unsigned int i;
-    int j;
+	unsigned int n = va_arg(args, unsigned int);
+	char *binary = malloc(sizeof(char) * 33);
+	unsigned int i;
+	int j;
 
-    if (!binary)
-        return (-1);
-    for (i = 0; n > 0; i++)
-    {
-        binary[i] = (n % 2) + '0';
-        n /= 2;
-    }
-    binary[i] = '\0';
-    for (j = i - 1; j >= 0; j--)
-    {
-        _putchar(binary[j]);
-    }
-    free(binary);
-    return (i);
+	if (!binary)
+		return (-1);
+	for (i = 0; n > 0; i++)
+	{
+		binary[i] = (n % 2) + '0';
+		n /= 2;
+	}
+	binary[i] = '\0';
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(binary[j]);
+	}
+	free(binary);
+	return (i);
 }
 
