0ff9b47249d8d5bef26336166051b9e4d236bc4e
kilokomuli <kilokoemma175@gmail.com>
2023-09-18 12:39:08 -0700

    update

--- functions.c
+++ functions.c
@@ -85,36 +85,6 @@ int print_d(va_list args)
 		countnum /= 10;
 	}
 	return (count);
-}
-/**
- * binary - print a binary number
- *@va:unsigned integer input
- *
- * Return: no return
- */
-int print_binary(va_list args)
-{
-	unsigned int c;
-	int  i, j;
-	int arr[100];
-
-	c = va_arg(va, int);
-	i = 0;
-	if (c == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-	while (c > 0)
-	{
-		arr[i] = c % 2;
-		c = c / 2;
-		i++;
-	}
-	for (j = i - 1; j >= 0; j--)
-		_putchar(arr[j] + '0');
-	return (i);
-}
 /**
  * print_i - prints integer
  * @args: integer argument
